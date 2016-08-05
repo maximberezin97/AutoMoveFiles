286007048fffce5736feb3829b10724665ad4325
Maxim Berezin <maximberezin97@gmail.com>
2016-08-04 23:36:30 -0400

    Add links to useful webpages (Python docs, Google API tutorials).

--- AutoMoveFiles.py
+++ AutoMoveFiles.py
@@ -11,3 +11,10 @@ print('Target: ', targetInput)
 print('Target exists? ', os.path.exists(targetInput))
 print('Target is file? ', os.path.isfile(targetInput))
 print('Target is dir? ', os.path.isdir(targetInput))
+if os.path.isdir(targetInput):
+    print('List files: ', os.listdir(targetInput))
+
+# https://docs.python.org/2/library/os.html
+# https://docs.python.org/3/library/os.path.html
+# https://developers.google.com/api-client-library/python/apis/customsearch/v1
+# https://cloud.google.com/appengine/docs/python/
