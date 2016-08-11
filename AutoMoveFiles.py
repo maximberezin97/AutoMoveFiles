d2177108d4d8b78a475a4b8dad3b6dae426ae874
Maxim Berezin <maximberezin97@gmail.com>
2016-08-10 23:50:31 -0400

    Add .rar extract, parse formatted name, and Google API modules. Need to configure so that it installs in a virtualenv through pip (setup.py?). Add ability to extract and handle .rar files. Add proper handling of directories. Loses file structure, may pose contextual problems. Add information parser into video handler. Will use to sort movie/television content. May also use to sort comic content, will need to add 'issue' functionality to PTN. Add comic regex renamer. Add fields for Google API authentication. Will use to find album artwork for audio content. Add function to reliably separate superdirectory from current target, file or dir. Add .rar video test content.

--- AutoMoveFiles.py
+++ AutoMoveFiles.py
@@ -2,6 +2,41 @@ import sys
 import os
 import shutil
 import tempfile
+import re
+import modules.rarfile_28.rarfile as rarfile
+import modules.parse_torrent_name_100.PTN as ptn
+
+
+def get_superdir_and_file(file):
+    return os.path.split(os.path.normpath(file))
+
+
+def handle_file(target_file):
+    print('Handling file', target_file)
+    extension = os.path.splitext(target_file)[1]
+    print('Extension is', extension)
+    if rarfile.is_rarfile(target_file):
+        target_rar = rarfile.RarFile(target_file)
+        superdir, filename = get_superdir_and_file(target_file)
+        extract_path = os.path.join(superdir, os.path.splitext(filename)[0])
+        print('Extracting', target_file, '->', extract_path)
+        target_rar.extractall(extract_path)
+        handle_dir(extract_path)
+    elif extension in audio_types:
+        handle_audio_file(target_file)
+    elif extension in video_types:
+        handle_video_file(target_file)
+    elif extension in comic_types:
+        handle_comic_file(target_file)
+    else:
+        print('Unsupported file type.')
+
+
+def handle_dir(target_dir):
+    print('Handling directory', target_dir)
+    for dirpath in os.walk(target_dir):
+        for filename in dirpath[2]:
+            handle_file(os.path.join(dirpath[0], filename))
 
 
 def handle_audio_file(audio_file):
@@ -10,10 +45,23 @@ def handle_audio_file(audio_file):
 
 def handle_video_file(video_file):
     print('Handling video file', video_file)
+    parsed_info = ptn.parse(os.path.splitext(get_superdir_and_file(video_file)[1])[0])
+    print(parsed_info)
 
 
 def handle_comic_file(comic_file):
     print('Handling comic file', comic_file)
+    regex = re.compile('\(.*\)')
+    superdir, file = get_superdir_and_file(comic_file)
+    name, ext = os.path.splitext(file)
+    regexed_name = regex.sub('', name).strip(' ')
+    comic_file_renamed = os.path.join(superdir, regexed_name+ext)
+    print('Renaming', comic_file, '->', comic_file_renamed)
+    shutil.move(comic_file, comic_file_renamed)
+    comic_file_dest = os.path.join(dest_comic, regexed_name+ext)
+    print('Moving', comic_file_renamed, '->', comic_file_dest)
+    shutil.move(comic_file_renamed, dest_comic)
+
 
 
 # Declaring constants
@@ -23,36 +71,28 @@ comic_types = {'.cbr', '.cbz', '.pdf'}
 dest_audio = os.path.join('destination', 'audio')
 dest_video = os.path.join('destination', 'video')
 dest_comic = os.path.join('destination', 'comic')
+google_api_key = ''
+google_custom_search_engine_id = ''
 
 print('Argument list:', str(sys.argv))
 if len(sys.argv) >= 3:
+    if len(sys.argv) >= 5:
+        google_api_key = sys.argv[3]
+        google_custom_search_engine_id = sys.argv[4]
     path_input = sys.argv[1]
     file_input = sys.argv[2]
     target_input = os.path.join(path_input, file_input)
     if os.path.exists(target_input):
         with tempfile.TemporaryDirectory() as temp_dir:
-            target_temp = os.path.join(temp_dir, os.path.basename(os.path.normpath(target_input)))
+            target_temp = os.path.join(temp_dir, get_superdir_and_file(target_input)[1])
             if os.path.isfile(target_input):
                 print('Copying file', target_input, '->', target_temp)
                 shutil.copyfile(target_input, target_temp)
-                extension = os.path.splitext(target_temp)[1]
-                print('Extension is', extension)
-                if extension in audio_types:
-                    handle_audio_file(target_temp)
-                elif extension in video_types:
-                    handle_video_file(target_temp)
-                elif extension in comic_types:
-                    handle_comic_file(target_temp)
-                else:
-                    print('Unsupported file type.')
+                handle_file(target_temp)
             elif os.path.isdir(target_input):
                 print('Copying directory', target_input, '->', target_temp)
                 shutil.copytree(target_input, target_temp)
-                walk_list = os.walk(target_temp)
-                for item in walk_list:
-                    print('dirpath:', item[0])
-                    print('dirnames:', item[1])
-                    print('filenames:', item[2])
+                handle_dir(target_temp)
     else:
         print(target_input, 'does not exist.')
 else:
@@ -64,3 +104,4 @@ else:
 # https://docs.python.org/2/library/shutil.html
 # https://developers.google.com/api-client-library/python/apis/customsearch/v1
 # https://cloud.google.com/appengine/docs/python/
+# https://automatetheboringstuff.com/chapter9/
---
+++ modules/parse_torrent_name_100/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    description = f.read()
+
+setup(
+    name='parse-torrent-name',
+    version=__import__('PTN').__version__,
+    author=__import__('PTN').__author__,
+    author_email=__import__('PTN').__email__,
+    license=__import__('PTN').__license__,
+    url='https://github.com/divijbindlish/parse-torrent-name',
+    description='Parse torrent name of a movie or TV show',
+    long_description=description,
+    packages=['PTN'],
+    keywords=('parse parser torrent torrents name names proper rename '
+              'movie movies tv show shows series extract find quality '
+              'group codec audio resolution title season episode year '
+              'information filename filenames file files meaningful'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Text Processing'
+    ]
+)
